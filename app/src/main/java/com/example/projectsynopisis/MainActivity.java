ab54ec4c9ec9a5fdb4365c378f892ee772f0eacf
Paras Kamboj <paras1234567899@gmail.com>
2024-02-15 04:17:37 +0530

    photo accessibility

--- app/src/main/java/com/example/projectsynopisis/MainActivity.java
+++ app/src/main/java/com/example/projectsynopisis/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -15,7 +16,8 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseAuth auth;
-    ImageView googlemap;
+    ImageView googlemap,mainimage;
+    Button button1,button2,button3;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +27,31 @@ public class MainActivity extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
 
         googlemap = findViewById(R.id.googlemap);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        mainimage = findViewById(R.id.mainimage);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mainimage.setImageResource(R.drawable.im1);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mainimage.setImageResource(R.drawable.secondimage);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mainimage.setImageResource(R.drawable.thirdimage);
+            }
+        });
 
         if(auth.getCurrentUser()==null){
             Intent intent = new Intent(MainActivity.this,login.class);
